a1a2fc379d2daa605160cef2b052182cfbb4a9a1
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-15 14:06:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,9 +12,8 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     puts phrase
-
-    if counter < number_of_times
       counter +=1
+    if counter < number_of_times
       break
     end
 
