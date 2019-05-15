a9c532aabd04e33da98becbf5024a6ca4a18062c
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-15 14:04:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
 counter = 0
   loop do
-    puts phrase
+    return phrase
     if counter < number_of_times
       break
     end
